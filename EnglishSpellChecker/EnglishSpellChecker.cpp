98c1a0264a53156a1c67d68d7800c5088c4185fe
Strausz <kirito1169@gmail.com>
2021-10-22 00:34:08 +0200

    Finished up the project

--- EnglishSpellChecker/EnglishSpellChecker.cpp
+++ EnglishSpellChecker/EnglishSpellChecker.cpp
@@ -1,4 +1,9 @@
+#include "Node.h"
+#include "Tire.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
 std::vector<std::string> SplitSentence(const std::string sentence)
 {
 	std::vector<std::string> words;
@@ -13,5 +18,37 @@ std::vector<std::string> SplitSentence(const std::string sentence)
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Trie trieTree;
+	std::ifstream inputFile("wordsEn.txt");
+
+
+	if (inputFile.is_open())
+	{
+		std::string s;
+		while (!inputFile.eof())
+		{
+			std::getline(inputFile, s);
+			if (s.find('\'') == std::string::npos)
+			{
+				trieTree.Insert(s);
+			}
+		}
+	}
+
+	inputFile.close();
+	std::cout << "Please give me the sentence you want to gramatically check: " << std::endl;
+	std::string sentence;
+	std::getline(std::cin, sentence);
+
+	std::vector<std::string> words = SplitSentence(sentence);
+
+	bool isCorrect = true;
+	int index = 0;
+	while (isCorrect == true && index < words.size())
+	{
+		isCorrect = trieTree.ChechIfCorrectWord(words[index]);
+		index++;
+	}
+
+	isCorrect ? std::cout << "Your sentence is gramatically correct!" << std::endl : std::cout << "The sentence is gramatically not correct!" << std::endl;
 }
