d3783a50693f2ccc125e367841464ad0df5188eb
Strausz <kirito1169@gmail.com>
2021-10-22 00:27:06 +0200

    Created an auxiliary function for splitting the sentences

--- EnglishSpellChecker/EnglishSpellChecker.cpp
+++ EnglishSpellChecker/EnglishSpellChecker.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+std::vector<std::string> SplitSentence(const std::string sentence)
+{
+	std::vector<std::string> words;
+	std::string actWord = "";
+
+	for (auto character : sentence)
+		(character == ' ') ? words.push_back(actWord), actWord = "" : actWord += character;
+	words.push_back(actWord);
+
+	return words;
+}
 
 int main()
 {
